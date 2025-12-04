285517f3a89d451b7dc0a2abf2e826bc9742553c
Winicius Torres <rm366458@fiap.com.br>
2025-12-04 19:55:47 -0300

    feat: criação de novo endpoint de contrato

--- src/Fiap.FCG.User.WebApi/Usuarios/Consultar/Grpc/UsuarioGrpcService.cs
+++ src/Fiap.FCG.User.WebApi/Usuarios/Consultar/Grpc/UsuarioGrpcService.cs
@@ -1,13 +1,15 @@
-using System.Threading.Tasks;
 using Fiap.FCG.User.Application.Usuarios.Consultar.ComNotificacaoAtiva;
+using Fiap.FCG.User.Domain.Usuarios;
 using Fiap.FCG.User.WebApi.Protos;
 using Grpc.Core;
+using System.Threading.Tasks;
 
 namespace Fiap.FCG.User.WebApi.Usuarios.Consultar.Grpc;
 
 public class UsuarioGrpcService : UsuarioService.UsuarioServiceBase
 {
     private readonly IConsultaComNotificacaoAtivaQuery _query;
+    private readonly IUsuarioRepository _usuarioRepository;
 
     public UsuarioGrpcService(IConsultaComNotificacaoAtivaQuery query)
     {
@@ -15,13 +17,13 @@ public class UsuarioGrpcService : UsuarioService.UsuarioServiceBase
     }
 
     public override async Task<ObterUsuariosComNotificacoesResponse> ObterUsuariosComNotificacoes(
-        ObterUsuariosComNotificacoesRequest request, 
+        ObterUsuariosComNotificacoesRequest request,
         ServerCallContext context)
     {
         var usuarios = await _query.ExecuteAsync();
 
         var response = new ObterUsuariosComNotificacoesResponse();
-        
+
         foreach (var usuario in usuarios)
         {
             response.Usuarios.Add(new UsuarioDto
@@ -33,5 +35,31 @@ public class UsuarioGrpcService : UsuarioService.UsuarioServiceBase
         }
 
         return response;
+    }
+
+    public override async Task<ObterUsuarioPorIdResponse> ObterUsuarioPorId(
+    ObterUsuarioPorIdRequest request,
+    ServerCallContext context)
+    {
+        var usuario = await _usuarioRepository.ObterPorIdAsync(request.UsuarioId);
+
+        if (usuario == null)
+        {
+            return new ObterUsuarioPorIdResponse
+            {
+                Existe = false
+            };
+        }
+
+        return new ObterUsuarioPorIdResponse
+        {
+            Existe = true,
+            Usuario = new UsuarioDto
+            {
+                Id = usuario.Id,
+                Nome = usuario.Nome,
+                Email = usuario.Email
+            }
+        };
     }
 }
